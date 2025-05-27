c41007a3c0361c57dd2959b00759eb545826b4a4
Timrosinnus64 <124527029+Timrosinnus64@users.noreply.github.com>
2025-05-27 09:11:23 +0200

    Fix __init__.py

--- ballsdex/packages/collector/__init__.py
+++ ballsdex/packages/collector/__init__.py
@@ -1,11 +1,9 @@
 from typing import TYPE_CHECKING
 
-from .cog import claim
+from ballsdex.packages.collector.cog import collector
 
 if TYPE_CHECKING:
     from ballsdex.core.bot import BallsDexBot
 
-
 async def setup(bot: "BallsDexBot"):
-    await bot.add_cog(claim(bot)) 
-    
+    await bot.add_cog(collector(bot))
